fb603ad1384766b755dadc63b1903fed0660b7f4
Bas Tolen <bas@bastolen.nl>
2019-11-29 20:13:13 +0100

    Added start of table view cell

---
+++ ELBHO Adviseurs/CustomTableViewCell/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  ELBHO Adviseurs
+//
+//  Created by Bas Tolen on 29/11/2019.
+//  Copyright © 2019 Otters. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

d1515cfda87aac44adf1f41a46d89b1e01e310cf
Kevin Bosma <kbosma98@gmail.com>
2019-12-01 19:37:46 +0100

    Start beschikbaarheid

---
+++ ELBHO Adviseurs/ViewControllers/BeschikbaarheidViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BeschikbaarheidViewController.swift
+//  ELBHO Adviseurs
+//
+//  Created by Kevin Bosma on 01/12/2019.
+//  Copyright © 2019 Otters. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import SwiftKeychainWrapper
+import MaterialComponents
+import SideMenu
+
+class BeschikbaarheidViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Beschikbaarheid"
+    }
+}

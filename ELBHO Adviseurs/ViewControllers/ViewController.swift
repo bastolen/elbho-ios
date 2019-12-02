7e0279e92fdbc90f18d7c56b45d375539d450666
Bas Tolen <bas@bastolen.nl>
2019-12-02 09:02:07 +0100

    Added images to labels

--- ELBHO Adviseurs/ViewControllers/ViewController.swift
+++ ELBHO Adviseurs/ViewControllers/ViewController.swift
@@ -16,13 +16,22 @@ class ViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
     let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    let selectedBarImages: [UIImage?] = [
+        UIImage(named: "OpenAgendaSelected"),
+        UIImage(named: "AcceptedAgendaSelected"),
+        UIImage(named: "DoneAgendaSelected"),
+    ]
+    let barImages: [UIImage?] = [
+        UIImage(named: "OpenAgenda"),
+        UIImage(named: "AcceptedAgenda"),
+        UIImage(named: "DoneAgenda")
+    ]
     
     @IBOutlet weak var Container: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Afspraken"
-        
         if(!KeychainWrapper.standard.hasValue(forKey: "authToken")) {
             // Not logged in, show login screen
             navigationController?.setViewControllers([mainStoryboard.instantiateViewController(identifier: "LoginViewController")], animated:true)
@@ -42,11 +51,12 @@ class ViewController: UIViewController {
     
     private func setupTabBar() {
         // Create the items
-        let itemOpen = UITabBarItem(title: "agenda_open".localize, image: UIImage(named: "AcceptedAgenda"), selectedImage: UIImage(named: "AcceptedAgendaSelected"))
+        let itemOpen = UITabBarItem(title: "agenda_open".localize, image: selectedBarImages[0], tag: 0)
         itemOpen.badgeValue = "5"
-        let itemAccepted = UITabBarItem(title: "agenda_accepted".localize, image: UIImage(named: "OpenAgenda"), selectedImage: UIImage(named: "OpenAgendaSelected"))
-        let itemDone = UITabBarItem(title: "agenda_done".localize, image: UIImage(named: "DoneAgenda"), selectedImage: UIImage(named: "DoneAgendaSelected"))
         
+        let itemAccepted = UITabBarItem(title: "agenda_accepted".localize, image: barImages[1], tag: 1)
+        
+        let itemDone = UITabBarItem(title: "agenda_done".localize, image: barImages[2], tag: 2)
         // Create the bar
         let tabBar = MDCTabBar(frame: view.bounds)
         tabBar.items = [
@@ -55,12 +65,40 @@ class ViewController: UIViewController {
             itemDone,
         ]
         tabBar.itemAppearance = .titledImages
-        tabBar.setTitleColor(UIColor(named: "Disabled"), for: .normal)
+        tabBar.setTitleColor(UIColor(named: "DisabledText"), for: .normal)
         tabBar.setTitleColor(UIColor(named: "Primary"), for: .selected)
-        tabBar.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
+        tabBar.delegate = self
         tabBar.sizeToFit()
         
         // Add the bar
         Container.addSubview(tabBar)
+    }
+}
+
+extension ViewController: MDCTabBarDelegate {
+    func tabBar(_ tabBar: MDCTabBar, didSelect item: UITabBarItem) {
+        for (index, element) in tabBar.items.enumerated() {
+            if element == item {
+                element.image = selectedBarImages[index]
+            } else {
+                element.image = barImages[index]
+            }
+        }
+// https://www.raywenderlich.com/316-google-material-design-tutorial-for-ios-getting-started
+// Check this
+        switch item.tag {
+        case 0:
+            print("agenda_open".localize)
+            break
+        case 1:
+            print("agenda_accepted".localize)
+            break
+        case 2:
+            print("agenda_done".localize)
+            break
+        default:
+            print("something else")
+            break
+        }
     }
 }

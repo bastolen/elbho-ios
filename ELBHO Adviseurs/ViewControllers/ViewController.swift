e389b6f4c61fda968a12951f77ac2e929d40381b
Bas Tolen <bas@bastolen.nl>
2019-12-05 10:15:48 +0100

    Added tabbar and datelabel

--- ELBHO Adviseurs/ViewControllers/ViewController.swift
+++ ELBHO Adviseurs/ViewControllers/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     private let disposeBag = DisposeBag()
     let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
     
+    @IBOutlet weak var TabBar: UITabBar!
+    @IBOutlet weak var DateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Afspraken"
@@ -27,6 +30,31 @@ class ViewController: UIViewController {
         }
         
         setupMenu()
+        setupDateLabel()
+        setupTabBar()
+    }
+    
+    private func setupTabBar() {
+        let tabItem1 = UITabBarItem(title: "agenda_open".localize, image: UIImage(named: "OpenAgenda"), selectedImage: UIImage(named: "OpenAgendaSelected"))
+        tabItem1.tag = 0
+        tabItem1.badgeValue = "1"
+        
+        let tabItem2 = UITabBarItem(title: "agenda_accepted".localize, image: UIImage(named: "AcceptedAgenda"), selectedImage: UIImage(named: "AcceptedAgendaSelected"))
+        tabItem2.tag = 1
+        
+        let tabItem3 = UITabBarItem(title: "agenda_done".localize, image: UIImage(named: "DoneAgenda"), selectedImage: UIImage(named: "DoneAgendaSelected"))
+        tabItem3.tag = 2
+        
+        TabBar.items = [tabItem1, tabItem2, tabItem3]
+        TabBar.selectedItem = tabItem1
+        TabBar.delegate = self
+    }
+    
+    private func setupDateLabel() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMMM yyyy"
+        DateLabel.text = formatter.string(from: date)
     }
     
     private func setupMenu()
@@ -34,5 +62,11 @@ class ViewController: UIViewController {
         let SideMenuView = mainStoryboard.instantiateViewController(identifier: "SideMenuView") as! SideMenuNavigationController
         SideMenuManager.default.leftMenuNavigationController = SideMenuView
         SideMenuView.statusBarEndAlpha = 0
+    }
+}
+
+extension ViewController: UITabBarDelegate {
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        print(item.tag)
     }
 }
